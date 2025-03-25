fd790978f019140e635c6d9ff2b8f8ea50ae924e
Tze <gantzesin@gmail.com>
2025-03-25 12:47:45 +0800

    Testing purpose on the collab

--- app/src/main/java/com/example/groupassignment/MainActivity.kt
+++ app/src/main/java/com/example/groupassignment/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Tester, welcome to the internet!",
         modifier = modifier
     )
 }
